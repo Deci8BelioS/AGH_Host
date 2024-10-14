e4b9436a0f4ffeb467180b471c471fe20a09b818
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2024-10-14 08:35:52 +0200

    Code converted to English

--- AGH/AGH_Whitelist.py
+++ AGH/AGH_Whitelist.py
@@ -1,237 +1,83 @@
 import requests, re
 
+from regex import DOMAIN_WHITELIST, DOMAIN_WHITELIST2, DOMAIN_WHITELIST3
+
 urls = [
     'https://raw.githubusercontent.com/celenityy/BadBlock/refs/heads/main/wildcards-no-star/whitelist.txt',
     'https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt'
 ]
 
-urls_sinfiltrar = [
+unfiltered_urls = [
     'https://raw.githubusercontent.com/swetoast/adguardhome-lists/refs/heads/main/whitelist.txt'
 ]
 
-output_file = '/home/runner/work/AGH_Host/AGH_Host/AGH/filters/whitelist/whitelist.txt'
+output_file = r'/home/runner/work/AGH_Host/AGH_Host/AGH/filters/whitelist/whitelist.txt'
 
-l1n34 = ['#', '!']
-l1n34_2 = ['apple.com', 's3.amazonaws.com', 'wp.com', 'amazonaws.com']
+l1n3 = ['#', '!']
+l1n3_2 = ['apple.com', 's3.amazonaws.com', 'wp.com', 'amazonaws.com']
 
-def descargar_archivo(url):
+def download_file(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.text.splitlines()
     except requests.exceptions.RequestException as e:
-        print(f"Error descargando {url}: {e}")
+        print(f"Error downloading '{url}': {e}")
         return []
-    
-REGEX_WHITELIST = [
-    "rain-alarm.com",
-    "activision.com",
-    "chomikuj.pl",
-    "firebasedynamiclinks.googleapis.com",
-    "epic.download",
-    "geotrust.com",
-    "cloudflare.com",
-    "evyy.net",
-    "adjust.com",
-    "alimentacionchino.com",
-    "cbsivideo.com",
-    "pluto.tv",
-    "onetrust.com",
-    "cbsi.com",
-    "tiqcdn.com",
-    "instagram.com",
-    "typekit.net",
-    "robertsspaceindustries.com",
-    "soundcloud.com",
-    "ipqualityscore.com",
-    "suyu.dev",
-    "plex.direct",
-    "revanced.net",
-    "atomixhq.baby",
-    "atresmedia.com",
-    "microsoft.com",
-    "leinad4mind.top",
-    "split.io",
-    "github.io",
-    "gitlab.com",
-    "raw.githubusercontent.com",
-    "digidip.net",
-    "chollometro.digidip.net",
-    "tradedoubler.com",
-    "plex.tv",
-    "fapality.com",
-    "virtualbox.org",
-    "ddos-guard.net",
-    "bbva.es",
-    "pccomponentes.com",
-    "es.spankbang.com",
-    "tinder.com",
-    "haiku-os.org",
-    "milanuncios.com",
-    "aliexpress.com",
-    "alicdn.com",
-    "go-mpulse.net",
-    "nsw2u.com",
-    "waaw.to",
-    "raidrive.com",
-    "netflixmirror.com",
-    "oraclecloud.com",
-    "eset.com",
-    "elamigos-games.net",
-    "mparticle.com",
-    "cookielaw.org",
-    "mmcdn.com",
-    "azure.com",
-    "nickchan.lol",
-    "ddnsfree.com",
-    "duckdns.org",
-    "dynu.com"
-]
-REGEX_WHITELIST2 = [
-    "3gppnetwork.org",
-    "microsoft.com",
-    "lencr.org",
-    "symantec.com",
-    "signal.org",
-    "t-mobile.com",
-    "tuyomovil.com",
-    "symcd.com",
-    "verisign.com",
-    "viero.com",
-    "symcb.com",
-    "zonamovil.com.pa",
-    "sectigo.com",
-    "geotrust.com",
-    "letsencrypt.org",
-    "mycricket.com",
-    "movistar.cl",
-    "movistar.com.ar",
-    "movistar.com.co",
-    "movistar.com.ec",
-    "movistar.com.uy",
-    "movistar.com.ve",
-    "movistar.com",
-    "movistar.cr",
-    "movistar.es",
-    "movistar.gt",
-    "movistar.mx",
-    "movistar.ni",
-    "movistar.pa",
-    "movistar.pe",
-    "movistar.sv",
-    "movistar.ve",
-    "3ireland.ie",
-    "accv.es",
-    "github.io",
-    "entrust.net",
-    "ocsp-responder.com",
-    "live.com",
-    "office.com",
-    "office.net",
-    "office365.com",
-    "wosign.com",
-    "globalsign.com",
-    "mopera.net",
-    "usertrust.com",
-    "trustwave.com",
-    "trust-provider.com",
-    "digicert.com",
-    "amazontrust.com",
-    "vodafone.al",
-    "vodafone.co.uk",
-    "vodafone.com.eg",
-    "vodafone.com.mt",
-    "vodafone.com.qa",
-    "vodafone.es",
-    "vodafone.gr",
-    "vodafone.hu",
-    "vodafone.ie",
-    "vodafone.is",
-    "vodafone.it",
-    "vodafone.net.nz",
-    "vodafone.pt",
-    "comodo.com",
-    "comodo.net",
-    "googleapis.com",
-    "orange.acte",
-    "orange.at",
-    "orange.cm",
-    "orange.co.uk",
-    "orange.com.do",
-    "orange.es",
-    "orange.fr",
-    "orange.lu",
-    "orange.mms",
-    "orange.ne",
-    "orange.nl",
-    "orange.pl",
-    "orange.re",
-    "orange.smartphone",
-    "orange.tn",
-    "orange.ug",
-    "orange.video",
-    "orange.web",
-    "orange.world",
-    "euskaltel.mobi",
-    "thawte.com",
-    "pepephone.com"
-]
 
-REGEX_WHITELIST3 = "mmsc."
+def line_filter(lines):
+    valid_domains = set()
+    for line in lines:
+        line = line.strip()
+        line = re.sub(r'^(0\.0\.0\.0|127\.0\.0\.1)\s+', '', line)
+        line = re.sub(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', '', line)
+        line = re.sub(r'##.*', '', line)
+        line = re.sub(r'#.*', '', line)
+        line = re.sub(r'www\.', '', line)
+        line = re.sub(r'\$.*$', '', line)
+        line = line.replace('http://', '').replace('https://', '').replace('@@||', '').replace('^', '').replace('@@|', '')
+        if line.startswith(tuple(l1n3)) or not line:
+            continue
+        if line.startswith(tuple(l1n3_2)) or not line:
+            continue
+        if line.startswith(DOMAIN_WHITELIST3):
+            continue
+        if line.endswith(tuple(DOMAIN_WHITELIST)) and not line.startswith(tuple(DOMAIN_WHITELIST)) or not line:
+            continue
+        if line.endswith(tuple(DOMAIN_WHITELIST2)) and not line.startswith(tuple(DOMAIN_WHITELIST2)) or not line:
+            continue
+        if not line.startswith('||') and not line.startswith('@@') and not line.startswith('|') and not line.startswith('@@/') and not line.endswith('$important'):
+            line = f'@@||{line}^$important'
+        if line:
+            valid_domains.add(line.strip())
+    return valid_domains
 
-def filtrar_lineas(lineas):
-    dominios_validos = set()
-    for linea in lineas:
-        linea = linea.strip()
-        linea = re.sub(r'^(0\.0\.0\.0|127\.0\.0\.1)\s+', '', linea)
-        linea = re.sub(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', '', linea)
-        linea = re.sub(r'##.*', '', linea)
-        linea = re.sub(r'#.*', '', linea)
-        linea = re.sub(r'www\.', '', linea)
-        linea = re.sub(r'\$.*$', '', linea)
-        linea = linea.replace('http://', '').replace('https://', '').replace('@@||', '').replace('^', '').replace('@@|', '')
-        if linea.startswith(tuple(l1n34)) or not linea:
+def unfiltered_lines(lines):
+    valid_domains = set()
+    for line in lines:
+        if line.startswith(tuple(l1n3)) or not line:
             continue
-        if linea.startswith(tuple(l1n34_2)) or not linea:
-            continue
-        if linea.startswith(REGEX_WHITELIST3):
-            continue
-        if linea.endswith(tuple(REGEX_WHITELIST)) and not linea.startswith(tuple(REGEX_WHITELIST)) or not linea:
-            continue
-        if linea.endswith(tuple(REGEX_WHITELIST2)) and not linea.startswith(tuple(REGEX_WHITELIST2)) or not linea:
-            continue
-        if not linea.startswith('||') and not linea.startswith('@@') and not linea.startswith('|') and not linea.startswith('@@/') and not linea.endswith('$important'):
-            linea = f'@@||{linea}^$important'
-        if linea:
-            dominios_validos.add(linea.strip())
-    return dominios_validos
+        line = line.strip()
+        if line:
+            valid_domains.add(line.strip())
+    return valid_domains
 
-def lineas__sinfiltrar(lineas):
-    dominios_validos = set()
-    for linea in lineas:
-        if linea.startswith(tuple(l1n34)) or not linea:
-            continue
-        linea = linea.strip()
-        if linea:
-            dominios_validos.add(linea.strip())
-    return dominios_validos
-
-contenido_unificado = set()
+unified_content = set()
 
 for url in urls:
-    print(f"Descargando desde {url}...")
-    lineas = descargar_archivo(url)
-    dominios_filtrados = filtrar_lineas(lineas)
-    contenido_unificado.update(dominios_filtrados)
+    print(f"Downloading from '{url}'...")
+    lines = download_file(url)
+    filtered_domains = line_filter(lines)
+    unified_content.update(filtered_domains)
 
-for url in urls_sinfiltrar:
-    print(f"Descargando desde {url}...")
-    lineas = descargar_archivo(url)
-    dominios_sinfiltrar = lineas__sinfiltrar(lineas)
-    contenido_unificado.update(dominios_sinfiltrar)
+for url in unfiltered_urls:
+    print(f"Downloading from '{url}'...")
+    lines = download_file(url)
+    unfiltered_domains = unfiltered_lines(lines)
+    unified_content.update(unfiltered_domains)
 
 with open(output_file, 'w', encoding='utf-8') as f:
-    for dominio in sorted(contenido_unificado):
-        f.write(dominio + '\n')
+    for domain in sorted(unified_content):
+        f.write(domain + '\n')
 
-print(f"Archivo '{output_file}' generado con éxito.")
+print(f"File '{output_file}' generated successfully.")
