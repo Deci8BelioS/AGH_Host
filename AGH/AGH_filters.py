3fa9f67acca0e69fb581859cd75515a0c0efea33
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2024-10-14 08:28:37 +0200

    Code converted to English

--- AGH/AGH_filters.py
+++ AGH/AGH_filters.py
@@ -1,67 +1,67 @@
 import requests, re
 
-from regex import REGEX, REGEX_WHITELIST, REGEX_WHITELIST2, SUBDOMINIO_DUPLICADO, SUBDOMINIO_DUPLICADO2, SUBDOMINIO_DUPLICADO3, SUBDOMINIO_DUPLICADO4
+from regex import REGEX, REGEX_WHITELIST, DOMAIN_LIST, SUBDOMAIN_DUPLICATE, SUBDOMAIN_DUPLICATE2, SUBDOMAIN_DUPLICATE3, SUBDOMAIN_DUPLICATE4
 
-output_file = 'hosts.txt'
+output_file = r'hosts.txt'
 
-l1n34 = ['#', '!', '-', '*', '/', '.', '&', '%', '~', '?', '[', '^', ':', '@']
+l1n3 = ['#', '!', '-', '*', '/', '.', '&', '%', '~', '?', '[', ']', '^', ':', '@', '<', 'fe80::', 'ff00::', 'ff02::']
 
-def descargar_archivo(url):
+def download_file(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.text.splitlines()
     except requests.exceptions.RequestException as e:
-        print(f"Error al descargar desde {url}: {e}")
+        print(f"Error downloading from '{url}': {e}")
         return []
 
-def limpiar_linea(linea):
-    linea = linea.strip()
-    linea = re.sub(r'^(0\.0\.0\.0|127\.0\.0\.1)\s+', '', linea)
-    linea = re.sub(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', '', linea)
-    linea = re.sub(r'##.*', '', linea)
-    linea = re.sub(r'#.*', '', linea)
-    linea = re.sub(r'www\.', '', linea)
-    linea = re.sub(r'\$.*$', '', linea)
-    linea = linea.replace('http://', '').replace('https://', '').replace('||', '').replace('|', '').replace('^', '')
-    return linea
+def clean_line(line):
+    line = line.strip()
+    line = re.sub(r'^(0\.0\.0\.0|127\.0\.0\.1)\s+', '', line)
+    line = re.sub(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', '', line)
+    line = re.sub(r'##.*', '', line)
+    line = re.sub(r'#.*', '', line)
+    line = re.sub(r'www\.', '', line)
+    line = re.sub(r'\$.*$', '', line)
+    line = line.replace('http://', '').replace('https://', '').replace('||', '').replace('|', '').replace('^', '')
+    return line
 
-def filtrar_lineas(lineas, excepciones):
-    dominios_normales = set()
-    dominios_excepcion = set()
-    for linea in lineas:
-        linea = limpiar_linea(linea)
-        if linea.startswith(tuple(l1n34)) or not linea:
+def filter_lines(lines, exceptions):
+    normal_domains = set()
+    exception_domains = set()
+    for line in lines:
+        line = clean_line(line)
+        if line.startswith(tuple(l1n3)) or not line:
             continue
-        if linea.startswith(f"{SUBDOMINIO_DUPLICADO}"):
-            dominios_normales.add(f"{SUBDOMINIO_DUPLICADO}*.*")
+        if line.startswith(f"{SUBDOMAIN_DUPLICATE}"):
+            normal_domains.add(f"{SUBDOMAIN_DUPLICATE}*.*")
             continue
-        if linea.endswith(f"{SUBDOMINIO_DUPLICADO2}"):
-            dominios_normales.add(f"*.{SUBDOMINIO_DUPLICADO2}")
+        if line.endswith(f"{SUBDOMAIN_DUPLICATE2}"):
+            normal_domains.add(f"*.{SUBDOMAIN_DUPLICATE2}")
             continue
-        if linea.startswith(f"{SUBDOMINIO_DUPLICADO3}"):
-            dominios_normales.add(f"{SUBDOMINIO_DUPLICADO3}*.eu")
+        if line.startswith(f"{SUBDOMAIN_DUPLICATE3}"):
+            normal_domains.add(f"{SUBDOMAIN_DUPLICATE3}*.eu")
             continue
-        if linea.endswith(f"{SUBDOMINIO_DUPLICADO4}"):
-            dominios_normales.add(f"{SUBDOMINIO_DUPLICADO4}")
+        if line.endswith(f"{SUBDOMAIN_DUPLICATE4}"):
+            normal_domains.add(f"{SUBDOMAIN_DUPLICATE4}")
             continue
-        if any(pattern.search(linea) for pattern in REGEX) or not linea:
+        if any(pattern.search(line) for pattern in REGEX) or not line:
             continue
-        if linea.endswith(tuple(REGEX_WHITELIST)) or not linea:
+        if line.endswith(tuple(REGEX_WHITELIST)) or not line:
             continue
-        if linea.endswith(tuple(REGEX_WHITELIST2)) and not linea.startswith(tuple(REGEX_WHITELIST2)) or not linea:
+        if line.endswith(tuple(DOMAIN_LIST)) and not line.startswith(tuple(DOMAIN_LIST)) or not line:
             continue
-        dominios_normales.add(linea.strip())
-    dominios_filtrados = dominios_normales - {d[2:] for d in dominios_excepcion}
-    dominios_filtrados.update(dominios_excepcion)
-    return dominios_filtrados
+        normal_domains.add(line.strip())
+    filtered_domains = normal_domains - {d[2:] for d in exception_domains}
+    filtered_domains.update(exception_domains)
+    return filtered_domains
 
-contenido_unificado = set()
-file_excepciones = "/home/runner/work/AGH_Host/AGH_Host/AGH/filters/whitelist/whitelist.txt"
+unified_content = set()
+file_exceptions = r"/home/runner/work/AGH_Host/AGH_Host/AGH/filters/whitelist/whitelist.txt"
 
-print(f"Leyendo desde archivo de excepciones: {file_excepciones}...\n")
-with open(file_excepciones, 'r', encoding='utf-8') as file:
-    excepciones = file.readlines()
+print(f"Reading from exception file: {file_exceptions}...\n")
+with open(file_exceptions, 'r', encoding='utf-8') as file:
+    exceptions = file.readlines()
 
 urls = [
     'https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/light.txt',
@@ -74,19 +74,19 @@ urls = [
 ]
 
 for url in urls:
-    print(f"Descargando desde {url}...")
-    lineas = descargar_archivo(url)
-    dominios_filtrados = filtrar_lineas(lineas, excepciones)
-    contenido_unificado.update(dominios_filtrados)
+    print(f"Downloading from '{url}'...")
+    lines = download_file(url)
+    filtered_domains = filter_lines(lines, exceptions)
+    unified_content.update(filtered_domains)
 
 with open(output_file, 'w', encoding='utf-8') as f:
-    for dominio in sorted(contenido_unificado):
-        if dominio.startswith('@@'):
-            dominio = f'{dominio}^'.replace('|^', '^|')
-        if not dominio.startswith('||') and not dominio.startswith('@@') and not dominio.startswith('|'):
-            dominio = f'||{dominio}'
-        if not dominio.endswith('^') and not dominio.endswith('^|') and not dominio.endswith('^$important'):
-            dominio = f'{dominio}^'.replace('$important^', '^$important')
-        f.write(f"{dominio}\n")
+    for domain in sorted(unified_content):
+        if domain.startswith('@@'):
+            domain = f'{domain}^'.replace('|^', '^|')
+        if not domain.startswith('||') and not domain.startswith('@@') and not domain.startswith('|') and not domain.startswith('<'):
+            domain = f'||{domain}'
+        if not domain.endswith('^') and not domain.endswith('^|') and not domain.endswith('^$important') and not domain.startswith('<'):
+            domain = f'{domain}^'.replace('$important^', '^$important')
+        f.write(f"{domain}\n")
 
-print(f"Archivo '{output_file}' generado con éxito.")
+print(f"File '{output_file}' generated successfully.")
